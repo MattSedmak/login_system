9d89df684c7a807c58193551c3b6e0640d22b56c
MattSedmak <sedmakmatt@gmail.com>
2021-02-20 10:18:56 +0100

    Added routes

---
+++ routes/loginRoute.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const route = express.Router();
+
+// Importfunctions
+const homeController = require("../controller/homeController");
+const registerController = require("../controller/registerController");
+const loginController = require("../controller/loginController");
+const resetController = require("../controller/resetController");
+
+// Start page
+route.get("/", homeController.showStart);
+// logout
+route.get("/logout", homeController.logout_get);
+
+// register
+route.get("/register", registerController.register_get);
+route.post("/register", registerController.register_post);
+
+// login
+route.get("/login", loginController.login_get);
+route.post("/login", loginController.login_post);
+
+// reset password
+route.get("/reset", resetController.reset_get);
+route.post("/reset", resetController.reset_post);
+
+//resetPasswordForm
+route.get("/reset/:cryptoUrl", resetController.verifyCryptoUrl_get);
+route.post("/resetPasswordForm", resetController.resetPasswordForm_post);
+
+// *** export ***
+module.exports = route;
---
+++ routes/todoRoute.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const route = express.Router();
+
+// Importfunctions
+const verifyUser = require("../middleware/verifyUser");
+const todoController = require("../controller/todoController");
+
+// Todopage
+route.get("/todolist", verifyUser, todoController.todolist_get);
+route.post("/todolist", verifyUser, todoController.addTodo_post);
+
+// Delete todo
+route.get("/todolist/delete/:id", verifyUser, todoController.deleteTodo_get);
+
+// Edit todo
+route.get("/todolist/edit/:id", verifyUser, todoController.editTodo_get);
+route.post("/todolist/edit/:id", verifyUser, todoController.editTodo_post);
+// *** export ***
+module.exports = route;
