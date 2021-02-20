2eb8216dabfacec79ef10fcb648067d41d032c10
MattSedmak <sedmakmatt@gmail.com>
2021-02-20 12:57:30 +0100

    Added styling for login and todo pages

--- controller/homeController.js
+++ controller/homeController.js
@@ -11,5 +11,5 @@ exports.showStart = (req, res) => {
 exports.logout_get = (req, res) => {
   res.clearCookie("jwtToken");
   req.flash("success_msg", "You are now logged out!");
-  res.redirect("/");
+  res.redirect("/login");
 };
